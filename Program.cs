80459a27acf058aab9a1a2710e94915ba8c921e6
Sunkaster <54435371+sunkaster@users.noreply.github.com>
2020-11-27 11:18:17 +0100

    Added Program.cs
    Added program.cs which is the first assignment for the programming olympics

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Äpplen_och_päron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int äpple = 7;
+            int päron = 13;
+            Console.WriteLine("Axels äpplen:");
+            int Axel = int.Parse(Console.ReadLine());
+            Console.WriteLine("Petras äpplen:");
+            int Petra = int.Parse(Console.ReadLine());
+
+            int AxelResultat = Axel * äpple;
+            int PetraResultat = Petra * päron;
+
+            if (AxelResultat < PetraResultat)
+            {
+                Console.WriteLine("Petra har tjänat mest");
+            }
+            else if (AxelResultat > PetraResultat)
+            {
+                Console.WriteLine("Axel har tjänat mest");
+            }
+            else
+            {
+                Console.WriteLine("Dem har tjänat lika mycket");
+            }
+        }
+    }
+}
